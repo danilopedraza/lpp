b875755c84aa44c66e756938c20cb3dd1b0e8460
César Pedraza <cpedraza@unal.edu.co>
2021-01-27 15:01:11 -0500

    Added basic assigments

--- lpp/token.py
+++ lpp/token.py
@@ -4,7 +4,10 @@ from enum import (
     unique
 )
 
-from typing import NamedTuple
+from typing import (
+    NamedTuple,
+    Dict
+)
 
 
 @unique
@@ -30,3 +33,11 @@ class Token(NamedTuple):
 
     def __str__(self) -> str:
         return f'Type: {self.token_type}, Literal: {self.literal}'
+
+
+def lookup_token_type(literal: str) -> TokenType:
+    keywords: Dict[str, TokenType] = {
+        'variable' :TokenType.LET
+    }
+
+    return keywords.get(literal, TokenType.IDENT)
