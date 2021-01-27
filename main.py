cfb33508bc6eb6f117504bd74c4c4de7a0542b4d
César Pedraza <cpedraza@unal.edu.co>
2021-01-27 16:52:02 -0500

    First version of REPL

---
+++ main.py
@@ -0,0 +1,11 @@
+from lpp.repl import start_repl
+
+
+def main() -> None:
+    print('LSP4000 (Lenguaje Super Vergón 4000)')
+    print('Escribe una sentencia.')
+
+    start_repl()
+
+if __name__ == '__main__':
+    main()
