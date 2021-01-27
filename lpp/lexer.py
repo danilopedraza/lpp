02e32c89ed0bfb253b853f866497dd0433d8c195
César Pedraza <cpedraza@unal.edu.co>
2021-01-27 14:14:20 -0500

    Added new token rules, tests passed

--- lpp/lexer.py
+++ lpp/lexer.py
@@ -1,3 +1,4 @@
+from re import match
 from lpp.token import (
     Token,
     TokenType
@@ -15,7 +16,27 @@ class Lexer:
 
     
     def next_token(self) -> Token:
-        token = Token(TokenType.ILLEGAL, self._character)
+        if match(r'^=$', self._character):
+            token = Token(TokenType.ASSIGN, self._character)
+        elif match(r'^\+$', self._character):
+            token = Token(TokenType.PLUS, self._character)
+        elif match(r'^$', self._character):
+            token = Token(TokenType.EOF, self._character)
+        elif match(r'^\($', self._character):
+            token = Token(TokenType.LPAREN, self._character)
+        elif match(r'^\)$', self._character):
+            token = Token(TokenType.RPAREN, self._character)
+        elif match(r'^{$', self._character):
+            token = Token(TokenType.LBRACE, self._character)
+        elif match(r'^}$', self._character):
+            token = Token(TokenType.RBRACE, self._character)
+        elif match(r'^,$', self._character):
+            token = Token(TokenType.COMMA, self._character)
+        elif match(r'^;$', self._character):
+            token = Token(TokenType.SEMICOLON, self._character)
+        else:
+            token = Token(TokenType.ILLEGAL, self._character)
+        
         self._read_character()
 
         return token
