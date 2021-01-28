b7ba01741740fccf81823f96923a84121df7af66
César Pedraza <cpedraza@unal.edu.co>
2021-01-28 17:09:57 -0500

    Implemented parser methods and a test

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -1,5 +1,8 @@
 from unittest import TestCase
-from lpp.ast import Program
+from lpp.ast import (
+    LetStatement,
+    Program
+)
 from lpp.lexer import Lexer
 from lpp.parser import Parser
 
@@ -14,4 +17,17 @@ class ParserTest(TestCase):
 
         self.assertIsNotNone(program)
         self.assertIsInstance(program, Program)
+    
+    def test_let_statements(self) -> None:
+        source: str = 'variable x = 5;\nvariable y = 10;\nvariable z = 20;'
+        lexer: Lexer = Lexer(source)
+        parser: Parser = Parser(lexer)
+
+        program: Program = parser.parse_program()
+
+        self.assertEqual(len(program.statements), 3)
+
+        for i in range(len(program.statements)):
+            self.assertEqual(str(program.statements[i])[:10], source.splitlines()[i][:10])
+            self.assertIsInstance(program.statements[i], LetStatement)
 
