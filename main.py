8f35d3a74bcea0e284848536ae71b9cfbf6d70e8
César Pedraza <cpedraza@unal.edu.co>
2021-01-28 15:10:33 -0500

    Added ASTNode class, corrected a typo

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from lpp.repl import start_repl
 
 
 def main() -> None:
-    print('LSP4000 (Lenguaje Super Vergón 4000)')
+    print('LSV4000 (Lenguaje Super Vergón 4000)')
     print('Escribe una sentencia.')
 
     start_repl()
